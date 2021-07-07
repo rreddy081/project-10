2e50cd0e6b8aaa7627e85acf2d5bf47399150fb2
rreddy081 <86635210+rreddy081@users.noreply.github.com>
2021-07-07 11:33:24 -0700

    Add files via upload

---
+++ Project_Template_C10_Travelling_in_Ship-main/sketch.js
@@ -0,0 +1,24 @@
+
+
+seaImg = loadImage(sea.png);
+shipImg1 = loadAnimation(ship-1.png);
+
+
+
+  
+
+
+
+function preload(){
+
+}
+
+function setup(){
+  createCanvas(400,400);
+  
+}
+
+function draw() {
+  background("blue");
+ 
+}
